b8ac717ff75a8516f0d7af45151eb119024abee7
Nishant Kumar <85111487+nk0129@users.noreply.github.com>
2022-08-24 12:31:01 +0530

    Time: 67 ms (5.01%), Space: 5.8 MB (88.51%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n<=0)return false;
-      int ldiv=(int)pow(3, floor(log(INT_MAX)/log(3)));
-       // cout<<ldiv<<endl;
+        if(n<=0)
+            return false;
+     // int ldiv=(int)pow(3, floor(log(INT_MAX)/log(3)));
+        int ldiv=1162261467;
+        cout<<ldiv<<endl;
         if(ldiv%n==0)
             return true;
         return false;
