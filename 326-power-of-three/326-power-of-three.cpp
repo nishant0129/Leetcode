fa253eb456634d71f63dfd25c32518cc08b82bed
Nishant Kumar <85111487+nk0129@users.noreply.github.com>
2022-08-24 12:31:30 +0530

    Time: 20 ms (88.73%), Space: 6 MB (29.00%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -5,7 +5,7 @@ public:
             return false;
      // int ldiv=(int)pow(3, floor(log(INT_MAX)/log(3)));
         int ldiv=1162261467;
-        cout<<ldiv<<endl;
+       // cout<<ldiv<<endl;
         if(ldiv%n==0)
             return true;
         return false;
