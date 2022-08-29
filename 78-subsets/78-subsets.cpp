99623a7f04618e53a6ea35396823fdcb4251fd71
Nishant Kumar <85111487+nk0129@users.noreply.github.com>
2022-08-29 23:22:04 +0530

    Time: 4 ms (57.64%), Space: 16.9 MB (8.45%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,23 +1,23 @@
-class Solution {
-public:
-    vector<vector<int>> subsets(vector<int>& nums)
-    {
-        vector<vector<int>>ans;
-        vector<int>res;
-        ans.push_back({});
-        for(int i=0; i<nums.size(); i++)
-        {
-            int n=ans.size();
-            for(int j=0; j<n; j++)
-            {
-                vector<int>v=ans[j];
-                v.push_back(nums[i]);
-                ans.push_back(v);
-            }
-        }
-        return ans;
-    }
-};
+// class Solution {
+// public:
+//     vector<vector<int>> subsets(vector<int>& nums)
+//     {
+//         vector<vector<int>>ans;
+//         vector<int>res;
+//         ans.push_back({});
+//         for(int i=0; i<nums.size(); i++)
+//         {
+//             int n=ans.size();
+//             for(int j=0; j<n; j++)
+//             {
+//                 vector<int>v=ans[j];
+//                 v.push_back(nums[i]);
+//                 ans.push_back(v);
+//             }
+//         }
+//         return ans;
+//     }
+// };
 
 
 // class Solution {
@@ -31,7 +31,7 @@ public:
 //               vector<int>v;
 //             for(int j=0; j<nums.size(); j++)
 //             {
-//               if(i& (1<<j))
+//                    if(i& (1<<j))
 //                     v.push_back(nums[j]) ;
                  
 //             }
@@ -40,4 +40,28 @@ public:
 //                  return ans;
 //    }
 // };
+
+class Solution {
+public:
+    void solve(vector<int>nums,int i,vector<int>out,vector<vector<int>>&ans)
+    {
+        if(i==nums.size())
+        {
+            ans.push_back(out);
+            return;
+        }
+        solve(nums,i+1,out,ans);
+        out.push_back(nums[i]);
+        solve(nums,i+1,out,ans);
+        
+    }
+    vector<vector<int>> subsets(vector<int>& nums)
+    {
+        vector<int>out;
+        vector<vector<int>>ans;
+        solve(nums,0,out,ans);
+        return ans;
+    }
+};
+
 
