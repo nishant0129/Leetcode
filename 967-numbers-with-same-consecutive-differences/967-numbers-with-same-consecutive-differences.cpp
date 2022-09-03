9105c4802c16699a7fd5f16d254534cfcb80f829
Nishant Kumar <85111487+nk0129@users.noreply.github.com>
2022-09-03 15:39:29 +0530

    Time: 13 ms (12.82%), Space: 8.8 MB (22.70%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -1,30 +1,29 @@
-class Solution {
-public:
-    void solve(string s,int n,int k,vector<int>&ans)
-    {
-        if(s.size()==n)
-        {
-            ans.push_back(stoi(s));
-            return;
-        }
-        for(int i=0;i<=9;i++)
-        {
-            // int back=s.back()-'0';
-            if(abs(s.back()-'0'-i)==k)
-            {
-                solve(s+to_string(i),n,k,ans);
-            }
-        }
-    }
-    vector<int> numsSameConsecDiff(int n, int k) {
-        vector<int>ans;
-        for(int i=1;i<=9;i++)
-        {
-            solve(to_string(i),n,k,ans);
-        }
-        return ans;
-    }
-};
+// class Solution {
+// public:
+//     void solve(string s,int n,int k,vector<int>&ans)
+//     {
+//         if(s.size()==n)
+//         {
+//             ans.push_back(stoi(s));
+//             return;
+//         }
+//         for(int i=0;i<=9;i++)
+//         {
+//             if(abs(s.back()-'0'-i)==k)
+//             {
+//                 solve(s+to_string(i),n,k,ans);
+//             }
+//         }
+//     }
+//     vector<int> numsSameConsecDiff(int n, int k) {
+//         vector<int>ans;
+//         for(int i=1;i<=9;i++)
+//         {
+//             solve(to_string(i),n,k,ans);
+//         }
+//         return ans;
+//     }
+// };
 
 
 // class Solution {
@@ -54,47 +53,32 @@ public:
 //     }
 // };
 
-// class Solution {
-// public:
-//     void solve(int n,int k,vector<int>&ans,string s,int j)
-//     {
-         
-//         if(s.size()==n)
-//         {
-//             ans.push_back(stoi(s));
-//             return;
-//         }
-    
-//           if(abs(k,))
-//         solve(n,k,ans,s,j-1);
-//        s.pop_back();
-       
-        
-//     }
-//     vector<int> numsSameConsecDiff(int n, int k) {
-//         vector<int>ans;
-//         string s="";
-//     int j=n;
-//    for(int i=1; i<=9;i++)
-//     {  
-//        s="";
-//        s+=to_string(i);
-//         solve(n,k,ans,s,j);
-//     }
-//         return ans;
-//     }
-// };
-// class Solution {
-// public:
-//     void solve(int curr_digit,vector<int>&ans,)
-//     {
-           
-//     }
-//     vector<int> numsSameConsecDiff(int n, int k) {
-//         vector<int>ans;
-//         vector<int>curr_num;
-//         for(int i=1;i<=9;i++) {
-            
-//         }
-//     }
-// };
+class Solution {
+public:
+    void solve(int digit,vector<int> &curr_num,int n,int k,vector<int>&ans) {
+        if(digit<0 or digit>9 or curr_num.size()>n) {
+            return ;
+        }
+        curr_num.push_back(digit);
+        if(curr_num.size()==n) {
+            int num=0;
+            for(int i=0;i<n;i++) {
+                num=curr_num[i]+num*10;
+            }     
+            ans.push_back(num);
+        }
+        solve(digit+k,curr_num,n,k,ans);
+        if(k!=0) {
+        solve(digit-k,curr_num,n,k,ans);
+        }
+        curr_num.pop_back(); // using backtracking makes the code highly efficient and also don't forget to pass the curr_num vector by reference
+    }
+    vector<int> numsSameConsecDiff(int n, int k) {
+        vector<int>ans;
+        vector<int>curr_num;
+        for(int i=1;i<=9;i++) {
+            solve(i,curr_num,n,k,ans);
+        }
+        return ans;
+    }
+};
