06a943823e94ad53993e96ac0cb1ce553f2f4cd8
Nishant Kumar <85111487+nk0129@users.noreply.github.com>
2022-09-03 13:55:11 +0530

    Time: 12 ms (15.42%), Space: 7.5 MB (74.00%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -1,29 +1,58 @@
 class Solution {
 public:
-    void solve(int digit,int num,int cnt,int n,int k,vector<int>&ans)
+    void solve(string s,int n,int k,vector<int>&ans)
     {
-       if(cnt==n)
-       {
-           ans.push_back(num);
-           return;
-       }
-       for(int i=0;i<=9;i++)
-       {
-           if(abs(digit-i)==k)
-           {
-               solve(i,num*10+i,cnt+1,n,k,ans);
-           }
-       }
+        if(s.size()==n)
+        {
+            ans.push_back(stoi(s));
+            return;
+        }
+        for(int i=0;i<=9;i++)
+        {
+            // int back=s.back()-'0';
+            if(abs(s.back()-'0'-i)==k)
+            {
+                solve(s+to_string(i),n,k,ans);
+            }
+        }
     }
     vector<int> numsSameConsecDiff(int n, int k) {
         vector<int>ans;
         for(int i=1;i<=9;i++)
         {
-            solve(i,i,1,n,k,ans);
+            solve(to_string(i),n,k,ans);
         }
         return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     void solve(int digit,int num,int cnt,int n,int k,vector<int>&ans)
+//     {
+//        if(cnt==n)
+//        {
+//            ans.push_back(num);
+//            return;
+//        }
+//        for(int i=0;i<=9;i++)
+//        {
+//            if(abs(digit-i)==k)
+//            {
+//                solve(i,num*10+i,cnt+1,n,k,ans);
+//            }
+//        }
+//     }
+//     vector<int> numsSameConsecDiff(int n, int k) {
+//         vector<int>ans;
+//         for(int i=1;i<=9;i++)
+//         {
+//             solve(i,i,1,n,k,ans);
+//         }
+//         return ans;
+//     }
+// };
 
 // class Solution {
 // public:
