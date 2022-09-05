1eb6a48d455880db290a43aa3cdddff56bc7142f
Nishant Kumar <85111487+nk0129@users.noreply.github.com>
2022-09-05 10:14:05 +0530

    Time: 38 ms (46.76%), Space: 11.8 MB (65.30%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
@@ -18,29 +18,53 @@ public:
 };
 */
 
+// class Solution {
+// public:
+//     void solve(Node* root,map<int,vector<int>>&mp,int level)
+//     {
+//         if(!root)
+//             return;
+//         mp[level].push_back(root->val);
+//          for (Node* child : root->children)
+//             solve(child,mp,level+1);
+            
+        
+//     }
+//     vector<vector<int>> levelOrder(Node* root) {
+//         map<int,vector<int>>mp;
+//         vector<vector<int>>ans;
+//         solve(root,mp,0);
+//         for(auto i:mp)
+//         {
+//             vector<int>v;
+//             for(auto j:i.second)
+//                 v.push_back(j);
+//             ans.push_back(v);
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
-    void solve(Node* root,map<int,vector<int>>&mp,int level)
+    void solve(Node* root,int level,vector<vector<int>>&ans)
     {
         if(!root)
             return;
-        mp[level].push_back(root->val);
+         if(ans.size()==level)
+             ans.push_back({root->val});
+         else
+          ans[level].push_back(root->val);
          for (Node* child : root->children)
-            solve(child,mp,level+1);
+            solve(child,level+1,ans);
             
         
     }
     vector<vector<int>> levelOrder(Node* root) {
-        map<int,vector<int>>mp;
+        
         vector<vector<int>>ans;
-        solve(root,mp,0);
-        for(auto i:mp)
-        {
-            vector<int>v;
-            for(auto j:i.second)
-                v.push_back(j);
-            ans.push_back(v);
-        }
-        return ans;
+        solve(root,0,ans);
+       return ans;
     }
 };
