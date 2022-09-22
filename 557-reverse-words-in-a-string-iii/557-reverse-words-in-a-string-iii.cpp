f7bad81aa67e68ab7d36420e290a8bc61bd45dd4
Nishant Kumar <85111487+nk0129@users.noreply.github.com>
2022-09-22 11:52:49 +0530

    Time: 43 ms (19.79%), Space: 9.5 MB (90.16%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,21 +1,39 @@
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         string ans="";
+//         string res;
+//         for(int i=0; i<s.size(); i++)
+//         {
+//             res="";
+//             while(i<s.size() && s[i]!=' ')
+//             {
+//                 res+=s[i];
+//                 i++;
+//             }
+//             reverse(res.begin(),res.end());
+//             ans+=res+" ";
+//         }
+//         ans.pop_back();
+//         // ans+=res;
+//         return ans;
+//     }
+// };
 class Solution {
 public:
+   
     string reverseWords(string s) {
-        string ans="";
-        string res;
-        for(int i=0; i<s.size(); i++)
+     int j=0;
+        for(int i=0; i<s.length(); i++)
         {
-            res="";
-            while(i<s.size() && s[i]!=' ')
+            
+            if(s[i]==' ')
             {
-                res+=s[i];
-                i++;
+                reverse(s.begin()+j,s.begin()+i);
+                j=i+1;
             }
-            reverse(res.begin(),res.end());
-            ans+=res+" ";
         }
-        ans.pop_back();
-        // ans+=res;
-        return ans;
+        reverse(s.begin()+j,s.end());
+        return s;
     }
 };
