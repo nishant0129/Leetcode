70573214334a5a836d89fb90ba6f6e68a8952ee7
Nishant Kumar <85111487+nk0129@users.noreply.github.com>
2022-10-02 14:46:09 +0530

    Time: 254 ms (63.32%), Space: 113.5 MB (92.66%) - LeetHub

--- 2028-find-missing-observations/2028-find-missing-observations.cpp
+++ 2028-find-missing-observations/2028-find-missing-observations.cpp
@@ -20,39 +20,55 @@ public:
         
         
 //     }
+//     vector<int> missingRolls(vector<int>& rolls, int mean, int n)
+//     {
+//         int sum=(n+rolls.size())*mean-accumulate(rolls.begin(),rolls.end(),0);
+    
+//         if(sum<=0)
+//             return {};
+//         vector<int>v,ans;
+//       //  cout<<"sum="<<sum<<endl;
+//         int k=sum/n;
+//         if(k>6)
+//         {
+//             return {};
+//         }
+//       //  cout<<"k="<<k<<endl;
+//         int left_sum=sum-k*(n);
+//        // cout<<"left="<<left_sum<<endl;
+     
+//             ans.assign(n,k);
+//                 int i=0;
+//                 while(i<ans.size() && left_sum--)
+//                 {
+//                     ans[i]+=1;
+//                     i++;
+//                 }
+//            // cout<<"leftt"<< left_sum<<endl;
+//                 if(left_sum==-1)
+//                 {
+//                     if(ans[0]>6 || ans[ans.size()-1]<1)
+//                         return {};
+//                     else
+//                         return ans;
+//                 }
+//                 else
+//                 return {};
+//     }
+// };
+    
+    
+    
     vector<int> missingRolls(vector<int>& rolls, int mean, int n)
     {
         int sum=(n+rolls.size())*mean-accumulate(rolls.begin(),rolls.end(),0);
-    
-        if(sum<=0)
+        if(sum<n || sum>6*n)
             return {};
-        vector<int>v,ans;
-      //  cout<<"sum="<<sum<<endl;
-        int k=sum/n;
-        if(k>6)
-        {
-            return {};
-        }
-      //  cout<<"k="<<k<<endl;
-        int left_sum=sum-k*(n);
-       // cout<<"left="<<left_sum<<endl;
-     
-            ans.assign(n,k);
-                int i=0;
-                while(i<ans.size() && left_sum--)
-                {
-                    ans[i]+=1;
-                    i++;
-                }
-           // cout<<"leftt"<< left_sum<<endl;
-                if(left_sum==-1)
-                {
-                    if(ans[0]>6 || ans[ans.size()-1]<1)
-                        return {};
-                    else
-                        return ans;
-                }
-                else
-                return {};
+         int rem=sum%n;
+         vector<int>ans(n,sum/n);
+        for(int i=0;i<rem;i++)
+            ans[i]+=1;
+        return ans;   
+       
     }
 };
