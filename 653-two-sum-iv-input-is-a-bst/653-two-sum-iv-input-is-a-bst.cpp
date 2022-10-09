b8c99d54738f0c0c605c2ae6f727f73f0b3f590b
Nishant Kumar <85111487+nk0129@users.noreply.github.com>
2022-10-09 21:10:28 +0530

    Time: 93 ms (28.23%), Space: 38.7 MB (33.49%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -15,9 +15,10 @@ public:
     bool findTarget(TreeNode* root, int k) {
         if(!root)
             return false;
+         
             if(mp.find(k-root->val)!=mp.end())
             return true;
-          mp[root->val]++;
+         mp[root->val]++;
         return findTarget(root->left,k)||findTarget(root->right,k);
      
     }
