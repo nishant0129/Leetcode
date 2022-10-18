e6c0e7e7d92e7f1811d5bc9e9ba77c1c0a005b07
Nishant Kumar <85111487+nk0129@users.noreply.github.com>
2022-10-18 21:25:35 +0530

    Time: 25 ms (27.63%), Space: 6.5 MB (77.40%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    string solve(int n,string s)
-    {
-        if(n==1) return s;
-        string ans="";
-        int i=0,j;
-        while(i<s.size())
-        {
-            j=i;
-            while(i<s.size() && s[i]==s[j])
-            {
-                i++;
-            }
-            ans+=to_string(i-j)+s[j];
-        }
-        return solve(n-1,ans);
-    }
     string countAndSay(int n) {
-        return solve(n,"1");
+        string ans="1";
+        for(int i=1;i<n;i++) {
+            string temp="";
+            char digit=ans[0];
+            int count=0;
+            for(int j=0;j<ans.size();j++) {
+                if(ans[j]==digit) {
+                    count++;
+                }else {
+                    temp+=to_string(count)+digit;
+                    digit=ans[j];
+                    count=1;
+                }
+            }
+            // cout<<ans<<endl;
+            temp+=to_string(count)+digit;
+            ans=temp;
+        }
+        return ans;
     }
-   
 };
