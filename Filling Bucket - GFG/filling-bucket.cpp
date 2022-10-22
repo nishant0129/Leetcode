cb43b7d214147b54508a0f99948d70f464696219
Nishant Kumar <85111487+nk0129@users.noreply.github.com>
2022-10-22 15:16:26 +0530

    Added solution - LeetHub

---
+++ Filling Bucket - GFG/filling-bucket.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int fillingBucket(int N) {
+        // code here
+        vector<int>dp(N+1,0);
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3; i<=N; i++)
+        {
+            dp[i]=(dp[i-2]+dp[i-1])%100000000;
+        }
+        return dp[N];
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.fillingBucket(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
