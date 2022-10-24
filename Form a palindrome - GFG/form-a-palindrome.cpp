43b1a3201b9c6f684e401310fa144c002fb97427
Nishant Kumar <85111487+nk0129@users.noreply.github.com>
2022-10-24 19:12:33 +0530

    Added solution - LeetHub

--- Form a palindrome - GFG/form-a-palindrome.cpp
+++ Form a palindrome - GFG/form-a-palindrome.cpp
@@ -28,12 +28,11 @@ class Solution{
         for(int i=1;i<=n;i++) {
             for(int j=1;j<=n;j++) {
                 if(s1[i-1]==s2[j-1]) {
-                        dp[i][j]=1+dp[i-1][j-1];
-                    
+                    dp[i][j]=1+dp[i-1][j-1];
                 }
                 else {
-                        dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                    }
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
                 ans=max(dp[i][j],ans);
             }
         }
