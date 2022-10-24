790e8cb1ac5284412e3f8d0a5070a205e7543bbc
Nishant Kumar <85111487+nk0129@users.noreply.github.com>
2022-10-24 19:05:59 +0530

    Added solution - LeetHub

--- Form a palindrome - GFG/form-a-palindrome.cpp
+++ Form a palindrome - GFG/form-a-palindrome.cpp
@@ -10,27 +10,41 @@ using namespace std;
 class Solution{
   public:
   
-   int recur(string s1,string s2,int i,int j,vector<vector<int>>&dp)
-   {
-       if(i>=s1.size() ||j>=s2.size())
-       return 0;
-       if(dp[i][j]!=-1)
-       return dp[i][j];
-       if(s1[i]==s2[j])
-       {
-           return dp[i][j]= 1+recur(s1,s2,i+1,j+1,dp);
-       }
-       return dp[i][j]=max(recur(s1,s2,i+1,j,dp),recur(s1,s2,i,j+1,dp));
-   }
+   int largest_subsequence(string s1,string s2)
+//   {
+//       if(i>=s1.size() ||j>=s2.size())
+//       return 0;
+//       if(dp[i][j]!=-1)
+//       return dp[i][j];
+//       if(s1[i]==s2[j])
+//       {
+//           return dp[i][j]= 1+recur(s1,s2,i+1,j+1,dp);
+//       }
+//       return dp[i][j]=max(recur(s1,s2,i+1,j,dp),recur(s1,s2,i,j+1,dp));
+//   }
+    {
+        int n=s1.size(),ans=0;
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=n;j++) {
+                if(s1[i-1]==s2[j-1]) {
+                        dp[i][j]=1+dp[i-1][j-1];
+                    
+                }
+                else {
+                        dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                    }
+                ans=max(dp[i][j],ans);
+            }
+        }
+        return ans;
+    }
     int countMin(string str){
-    //complete the function here
-      int n=str.size();
-    vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+     int n=str.size();
     string s1=str,s2=str;
     reverse(s2.begin(),s2.end());
-     int lp=recur(s1,s2,0,0,dp);
-   
-     return n-lp;
+    int lp=largest_subsequence(s1,s2);
+    return n-lp;
     }
 };
 
