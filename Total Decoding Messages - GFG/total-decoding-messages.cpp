ddebe0c3298dccd57b9eeb32435a740c2eb31e44
Nishant Kumar <85111487+nk0129@users.noreply.github.com>
2022-10-25 15:33:03 +0530

    Added solution - LeetHub

--- Total Decoding Messages - GFG/total-decoding-messages.cpp
+++ Total Decoding Messages - GFG/total-decoding-messages.cpp
@@ -6,33 +6,46 @@ using namespace std;
 class Solution {
 	public:
 	 int mod=1000000007;
-	    int recur(string str,int i,vector<int>&dp)
-	    {
-	        if(i==str.size())
-	        {
-	            return 1;
-	        }
-	        if(str[i]=='0')return 0;
-	        if(dp[i]!=-1)
-	        return dp[i];
-	        int single= recur(str,i+1,dp);
-	        int db=0;
-	        if(i<str.size()-1)
-	        {
-	           int c=stoi(str.substr(i,2));
-	           if(c>=1&&c<=26)
-	           db=recur(str,i+2,dp);
-	        }
+	   // int recur(string str,int i,vector<int>&dp)
+	   // {
+	   //     if(i==str.size())
+	   //     {
+	   //         return 1;
+	   //     }
+	   //     if(str[i]=='0')return 0;
+	   //     if(dp[i]!=-1)
+	   //     return dp[i];
+	   //     int single= recur(str,i+1,dp);
+	   //     int db=0;
+	   //     if(i<str.size()-1)
+	   //     {
+	   //        int c=stoi(str.substr(i,2));
+	   //        if(c>=1&&c<=26)
+	   //        db=recur(str,i+2,dp);
+	   //     }
 	        
-	        return dp[i]=( single%mod+db%mod)%mod;
+	   //     return dp[i]=( single%mod+db%mod)%mod;
 	        
-	    }
-		int CountWays(string str){
-		    // Code here
-		    string s="";
+	   // }
+		int CountWays(string str) {
 		    int n=str.size();
-		    vector<int>dp(n+1,-1);
-		    return recur(str,0,dp);
+		    vector<long long int>dp(n+1,0);
+		    if(str[0]==0) {
+		        return 0;
+		    }
+		    dp[0]=1;
+		    for(int i=0;i<n;i++) {
+		        if(str[i]=='0') {
+		            continue;
+		        }
+		        if(i+1<=n) {
+		            dp[i+1]=(dp[i]+dp[i+1])%mod;
+		        }
+		        if(i+2<=n and stoi(str.substr(i,2))<=26) {
+		            dp[i+2]=(dp[i]+dp[i+2])%mod;
+		        }
+		    }
+		    return dp[n]%mod;
 		}
 
 };
