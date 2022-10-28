b17bc67b8c11f77cc49a096a91f89477e944c9c1
Nishant Kumar <85111487+nk0129@users.noreply.github.com>
2022-10-28 21:04:35 +0530

    Time: 55 ms (34.35%), Space: 13.1 MB (24.79%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -57,17 +57,16 @@ public:
         vector<vector<int>>dp(m+1,vector<int>(n+1,0));
   
         
-        for(int i=0; i<m; i++)
+        for(int i=1; i<m+1; i++)
         {
-            for(int j=0; j<n; j++)
+            for(int j=1; j<n+1; j++)
             {
-                
-                if(s1[i]==s2[j])
+                if(s1[i-1]==s2[j-1])
                 {
-                    dp[i+1][j+1]=1+dp[i][j];
+                    dp[i][j]=1+dp[i-1][j-1];
                 }
                 else
-                    dp[i+1][j+1]=max(dp[i][j+1],dp[i+1][j]);
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
                    
             }
         }
