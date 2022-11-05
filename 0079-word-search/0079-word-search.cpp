b825a6dcd13eaaa83062a1c231c8d258d220ef20
Nishant Kumar <85111487+nk0129@users.noreply.github.com>
2022-11-05 10:36:41 +0530

    Time: 424 ms (75.69%), Space: 8.1 MB (47.23%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    bool dfs(int i,int j,vector<vector<char>>&grid,string &word,int k)
+    bool dfs(int m,int n,int i,int j,vector<vector<char>>&grid,string &word,int k,vector<vector<bool>>& is_visited)
     {
-        int m=grid.size(),n=grid[0].size();
         if(k==word.size())
             return true;
-        if(i<0||i>=m||j<0||j>=n||grid[i][j]=='*'||grid[i][j]!=word[k])
+        if(i<0||i>=m||j<0||j>=n||is_visited[i][j]||grid[i][j]!=word[k])
             return false;
         char x=grid[i][j];
-        grid[i][j]='*';
-        bool down=dfs(i+1,j,grid,word,k+1);
-        bool right=dfs(i,j+1,grid,word,k+1);
-        bool left=dfs(i-1,j,grid,word,k+1);
-        bool up=dfs(i,j-1,grid,word,k+1);
-        grid[i][j]=x;
+        is_visited[i][j]=true;
+        bool down=dfs(m,n,i+1,j,grid,word,k+1,is_visited);
+        bool right=dfs(m,n,i,j+1,grid,word,k+1,is_visited);
+        bool left=dfs(m,n,i-1,j,grid,word,k+1,is_visited);
+        bool up=dfs(m,n,i,j-1,grid,word,k+1,is_visited);
+        is_visited[i][j]=false;
         return up||down||right||left;  
     }
     bool exist(vector<vector<char>>& grid, string word) {
         int m=grid.size(),n=grid[0].size();
+        vector<vector<bool>>is_visited(m,vector<bool>(n,false));
         for(int i=0; i<m; i++)
         {
             for(int j=0; j<n; j++)
             {
-                if(dfs(i,j,grid,word,0))
+                if(dfs(m,n,i,j,grid,word,0,is_visited))
                     return  true;
             }
         }
