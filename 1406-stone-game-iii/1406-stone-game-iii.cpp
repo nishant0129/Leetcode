dfe8b3635ae7592a445f416f186f92368d663c0c
Nishant Kumar <85111487+nk0129@users.noreply.github.com>
2022-11-12 05:08:20 +0530

    Time: 2911 ms (5.29%), Space: 376.6 MB (5.14%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,13 +1,67 @@
+
+//using 2d
+
+// class Solution {
+// public:
+//     int solve(vector<int>&v,int idx,int alice,vector<vector<int>>&dp)
+//     {
+        
+//         if(idx >= v.size())
+//            return 0;
+        
+//         if(dp[idx][alice]!= -1)
+//             return dp[idx][alice];
+//          int sum=0;
+//         if(alice)
+//         {
+//             int ans=INT_MIN;
+//             for(int i=idx; i-idx<3 && i<v.size();i++)
+//             {
+//                 sum+=v[i];
+//                 ans=max(ans,sum+solve(v,i+1,!alice,dp));
+//             }
+//             dp[idx][alice]=ans;
+//         }
+//         else
+//         {
+//             int ans=INT_MAX;
+             
+//             for(int i=idx; i-idx<3 && i<v.size();i++)
+//             {
+//                // sum+=v[i];
+//                 ans=min(ans,solve(v,i+1,!alice,dp));
+//             }
+//             dp[idx][alice]=ans;
+//         }
+//         return dp[idx][alice];
+        
+//     }
+//     string stoneGameIII(vector<int>& sv) {
+//         //vector<vector<vector<int>>>dp(sv.size()+1,vector<vector<int>>(2,vector<int>(2,-1)));
+//         vector<vector<int>>dp(sv.size()+1,vector<int>(2,-1));
+//         int asum= solve(sv,0,1,dp);
+//         int tsum=accumulate(sv.begin(),sv.end(),0);
+//         int bsum=tsum-asum;
+//        // cout<<asum<<" "<<bsum<<endl;
+//         if(asum>bsum )
+//             return "Alice";
+//         else if(bsum>asum)
+//             return "Bob";
+//         else
+//             return "Tie";
+//     }
+// };
+
 class Solution {
 public:
-    int solve(vector<int>&v,int idx,int alice,vector<vector<int>>&dp)
+    int solve(vector<int>&v,int idx,int alice,int bob,vector<vector<vector<int>>>&dp)
     {
         
         if(idx >= v.size())
            return 0;
         
-        if(dp[idx][alice]!= -1)
-            return dp[idx][alice];
+        if(dp[idx][alice][bob]!= -1)
+            return dp[idx][alice][bob];
          int sum=0;
         if(alice)
         {
@@ -15,28 +69,28 @@ public:
             for(int i=idx; i-idx<3 && i<v.size();i++)
             {
                 sum+=v[i];
-                ans=max(ans,sum+solve(v,i+1,!alice,dp));
+                ans=max(ans,sum+solve(v,i+1,!alice,!bob,dp));
             }
-            dp[idx][alice]=ans;
+            dp[idx][alice][bob]=ans;
         }
-        else
+        if(bob)
         {
             int ans=INT_MAX;
              
             for(int i=idx; i-idx<3 && i<v.size();i++)
             {
                // sum+=v[i];
-                ans=min(ans,solve(v,i+1,!alice,dp));
+                ans=min(ans,solve(v,i+1,!alice,!bob,dp));
             }
-            dp[idx][alice]=ans;
+            dp[idx][alice][bob]=ans;
         }
-        return dp[idx][alice];
+        return dp[idx][alice][bob];
         
     }
     string stoneGameIII(vector<int>& sv) {
-        //vector<vector<vector<int>>>dp(sv.size()+1,vector<vector<int>>(2,vector<int>(2,-1)));
-        vector<vector<int>>dp(sv.size()+1,vector<int>(2,-1));
-        int asum= solve(sv,0,1,dp);
+        vector<vector<vector<int>>>dp(sv.size()+1,vector<vector<int>>(2,vector<int>(2,-1)));
+        //vector<vector<int>>dp(sv.size()+1,vector<int>(2,-1));
+        int asum= solve(sv,0,1,0,dp);
         int tsum=accumulate(sv.begin(),sv.end(),0);
         int bsum=tsum-asum;
        // cout<<asum<<" "<<bsum<<endl;
