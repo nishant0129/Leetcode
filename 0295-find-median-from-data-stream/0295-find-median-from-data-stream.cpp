de69eaca26d80b602bce11706e1d34516cefb43b
Nishant Kumar <85111487+nk0129@users.noreply.github.com>
2022-11-12 07:11:47 +0530

    Time: 508 ms (76.45%), Space: 144.9 MB (6.99%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,13 +1,3 @@
-
-//  About ordered set
-
-//  Ordered set is a policy based data structure that keeps the unique elements in sorted order. It performs all the operations as performed by the set data structure in STL in log(n) complexity and performs two additional operations also in log(n) complexity .
-// here we use 
-// find_by_order(k) : K-th element in a set (counting from zero).
-
-//here we need to maintain the elements in a sorted form to find the median
-// common files
-
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
@@ -37,10 +27,3 @@ public:
         return *s.find_by_order(n/2);
     }
 };
-
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder* obj = new MedianFinder();
- * obj->addNum(num);
- * double param_2 = obj->findMedian();
- */
