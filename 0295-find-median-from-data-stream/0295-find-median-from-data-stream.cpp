fe2fc1c712d0d6fa41765792a0970000c2617e38
Nishant Kumar <85111487+nk0129@users.noreply.github.com>
2022-11-12 07:16:28 +0530

    Time: 1077 ms (5.02%), Space: 144.8 MB (6.99%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,29 +1,29 @@
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-#define ordered_set tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update> 
+#define ordered_set tree<int, null_type,less_equal <int>, rb_tree_tag,tree_order_statistics_node_update>
 
 class MedianFinder {
 public:
+
+ordered_set s;
+MedianFinder() {
     
-    ordered_set s;
-    MedianFinder() {
-        
-    }
+}
+
+void addNum(int num) 
+{
+   s.insert(num);
+}
+
+double findMedian() {
     
-    void addNum(int num) 
+    int n=s.size();
+    if(n%2==0)
     {
-       s.insert(num);
-    }
-    
-    double findMedian() {
+        return (double)(*s.find_by_order(n/2)+*s.find_by_order(n/2-1))/2 ;
         
-        int n=s.size();
-        if(n%2==0)
-        {
-            return (double)(*s.find_by_order(n/2)+*s.find_by_order(n/2-1))/2 ;
-            
-        }
-        return *s.find_by_order(n/2);
     }
+    return *s.find_by_order(n/2);
+}
 };
