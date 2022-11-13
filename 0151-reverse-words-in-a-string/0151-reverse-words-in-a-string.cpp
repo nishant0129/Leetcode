9ec9a2061496d34b05b5c2da1b7cad24b6d26c12
Nishant Kumar <85111487+nk0129@users.noreply.github.com>
2022-11-13 14:27:24 +0530

    Time: 27 ms (18.27%), Space: 20.7 MB (29.74%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+       stringstream ss(s);
+        string res,word;
+        while(ss>>word)
+        {
+            res=word+" "+res;
+        }
+        res.pop_back();
+        return res;
+    }
+};
