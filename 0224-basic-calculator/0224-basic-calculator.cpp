241b914bfd509dd5764dbc225f75c51e0d7aa479
Nishant Kumar <85111487+nk0129@users.noreply.github.com>
2022-11-20 18:53:21 +0530

    Time: 6 ms (95.99%), Space: 8.1 MB (43.51%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.cpp
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -1,66 +1,119 @@
-class Solution {
+//using recursion
+
+// class Solution {
+// public:
+//    int get_num(string &s, int &i)
+//    {
+//         int num = 0;
+//         while (s[i] >= '0' && s[i] <= '9') 
+//         {
+//             num *= 10;
+//             num += (s[i++] - '0');
+//         }
+//         return num;
+//     }
+    
+    
+//     int get_next(string& s, int& i) 
+//     {
+//         while (s[i] == ' ') 
+//             ++i;
+//         int ans;
+//         if (s[i] == '(') 
+//         {
+//             ans = calculate(s, ++i);
+//         } 
+//         else 
+//         {
+//             ans = get_num(s, i);
+//         }
+//         return ans;
+//     }
+    
+    
+//     int calculate(string& s, int& i)
+//     {
+//         int sign = 1, ans = 0;
+//         while (i < s.size() && s[i] != ')')
+//         {
+//             if (s[i] == ' ') 
+//             { 
+//                 ++i;
+//                 continue;
+//             }
+//             if (s[i] >= '0' && s[i] <= '9')
+//             {
+//                 ans += get_num(s, i);
+//             }
+//             else if (s[i] == '+' || s[i] == '-')
+//             {
+//                 sign = s[i] == '+' ? 1 : -1;
+//                 ans += sign * get_next(s, ++i);
+//             } 
+//             else
+//             {
+//                 ans += calculate(s, ++i);
+//             }
+//         }
+//         i++;
+//         return ans;
+//     }
+    
+    
+//     int calculate(string s) 
+//     {
+//         int i = 0;
+//         return calculate(s, i);
+//     }
+// };
+
+
+//using stack
+
+class Solution
+{
 public:
-   int get_num(string &s, int &i)
-   {
-        int num = 0;
-        while (s[i] >= '0' && s[i] <= '9') 
-        {
-            num *= 10;
-            num += (s[i++] - '0');
-        }
-        return num;
-    }
-    
-    
-    int get_next(string& s, int& i) 
-    {
-        while (s[i] == ' ') 
-            ++i;
-        int ans;
-        if (s[i] == '(') 
-        {
-            ans = calculate(s, ++i);
-        } 
-        else 
-        {
-            ans = get_num(s, i);
-        }
-        return ans;
-    }
-    
-    
-    int calculate(string& s, int& i)
-    {
-        int sign = 1, ans = 0;
-        while (i < s.size() && s[i] != ')')
-        {
-            if (s[i] == ' ') 
-            { 
-                ++i;
-                continue;
-            }
-            if (s[i] >= '0' && s[i] <= '9')
-            {
-                ans += get_num(s, i);
-            }
-            else if (s[i] == '+' || s[i] == '-')
-            {
-                sign = s[i] == '+' ? 1 : -1;
-                ans += sign * get_next(s, ++i);
-            } 
-            else
-            {
-                ans += calculate(s, ++i);
-            }
-        }
-        i++;
-        return ans;
-    }
-    
-    
     int calculate(string s) 
     {
-        int i = 0;
-        return calculate(s, i);
+        int n = s.size();
+        stack<int> st;
+        int sum=0,sign=1;
+        
+        for(int i=0;i<n;i++)
+        {
+            char ch = s[i];
+            if(isdigit(ch))
+            {
+                int val=0;
+                while(i<n && isdigit(s[i]))
+                {
+                    val = val*10 + (s[i]-'0');
+                    i++;
+                }
+                i--;
+                val *= sign;
+                sum += val;
+                sign = 1;
+            }
+            else if(ch=='(')
+            {
+                st.push(sum);
+                st.push(sign);
+                sum=0;
+                sign = 1;
+            }
+            else if(ch==')')
+            {
+                sum *= st.top();
+                st.pop();
+                sum += st.top();
+                st.pop();
+            }
+            else if(ch=='-')
+            {
+                sign = -1;
+            }
+        }
+        return sum;
     }
 };
