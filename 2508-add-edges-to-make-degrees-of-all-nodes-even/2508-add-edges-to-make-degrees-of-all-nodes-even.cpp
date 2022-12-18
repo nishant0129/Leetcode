b5d376a4f18f1d86d1e10174cbc936e0d0ad0094
Nishant Kumar <85111487+nk0129@users.noreply.github.com>
2022-12-18 16:06:31 +0530

    Time: 1335 ms (50.00%), Space: 225 MB (10.00%) - LeetHub

---
+++ 2508-add-edges-to-make-degrees-of-all-nodes-even/2508-add-edges-to-make-degrees-of-all-nodes-even.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    bool isPossible(int n, vector<vector<int>>& edges) {
+        map<int,vector<int>>graph;
+         map<pair<int,int>,bool> exists;
+        for(auto v:edges)
+        {
+            int x = v[0];
+            int y = v[1];
+            exists[{x,y}]=true;
+            exists[{y,x}]=true;
+            graph[v[0]].push_back(v[1]);
+            graph[v[1]].push_back(v[0]);
+        }
+        
+        vector<int>od;
+        for(auto i:graph)
+        {
+          if(i.second.size()%2)
+              od.push_back(i.first);
+        }
+        int degree=od.size();
+       // cout<<degree<<endl;
+          if (degree == 0) 
+            return true;
+          else if (degree == 2)
+          {
+            int a = od[0];
+            int b = od[1];
+            for (int k = 1; k <= n; k++) 
+            {
+                if (!exists[{a,k}] && !exists[{b,k}])
+                    return true;
+                
+            }
+         } 
+         else if (degree == 4)
+         {
+           
+            int a = od[0];
+            int b = od[1];
+            int c = od[2];
+            int d = od[3];
+ if ((!exists[{a,b}] && !exists[{c,d}]) || (!exists[{a,c}] && !exists[{b,d}]) || (!exists[{a,d}] &&!exists[{b,c}])) 
+            {
+               return true;
+            }
+        } 
+        return false;         
+    }
+};
