3ec83bab8a1ea6c6564626c4e2b310a453501819
Nishant Kumar <85111487+nk0129@users.noreply.github.com>
2022-12-25 10:19:21 +0530

    Time: 10 ms (76.23%), Space: 9.3 MB (55.12%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,33 +1,60 @@
 class Solution {
 public:
-    void recur(vector<int>&v,vector<int>&nums,set<vector<int>>&s,vector<int>&vis)
+//     void recur(vector<int>&v,vector<int>&nums,set<vector<int>>&s,vector<int>&vis)
+//     {
+//         if(v.size()==nums.size())
+//         {
+//             s.insert(v);
+//             return;
+//         }
+//         for(int i=0; i<nums.size(); i++)
+//         {
+//             if(!vis[i])
+//             {
+//               v.push_back(nums[i]);
+//               vis[i]=1;
+//               recur(v,nums,s,vis);
+//               v.pop_back();
+//               vis[i]=0;
+//             }
+            
+            
+//         }
+//     }
+//     vector<vector<int>> permuteUnique(vector<int>& nums) {
+//         set<vector<int>>s;
+//         vector<vector<int>>ans;
+//         vector<int>v,vis(nums.size(),0);
+//        recur(v,nums,s,vis);
+//         for(auto i:s)
+//             ans.push_back(i);
+//         return ans;
+//     }
+    
+    
+
+    void recur(vector<int>nums,int idx,vector<vector<int>>&ans)
     {
-        if(v.size()==nums.size())
+        if(idx>=nums.size())
         {
-            s.insert(v);
+            ans.push_back(nums);
             return;
         }
-        for(int i=0; i<nums.size(); i++)
-        {
-            if(!vis[i])
-            {
-              v.push_back(nums[i]);
-              vis[i]=1;
-              recur(v,nums,s,vis);
-              v.pop_back();
-              vis[i]=0;
+         for (int i = idx; i < nums.size(); i++) 
+         {
+                if (i == idx || nums[i] != nums[idx]) 
+                {
+                    swap(nums[idx], nums[i]);
+                    recur(nums, idx + 1, ans);
+                }
             }
-            
-            
-        }
     }
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        set<vector<int>>s;
+ vector<vector<int>> permuteUnique(vector<int>& nums) {
+     
         vector<vector<int>>ans;
-        vector<int>v,vis(nums.size(),0);
-       recur(v,nums,s,vis);
-        for(auto i:s)
-            ans.push_back(i);
+        sort(nums.begin(),nums.end());
+        recur(nums,0,ans);
+       
         return ans;
     }
 };
