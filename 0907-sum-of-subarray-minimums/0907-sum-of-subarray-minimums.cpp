2febbd953704d9fdd184047223cb05fdbc53efa7
Nishant Kumar <85111487+nk0129@users.noreply.github.com>
2022-12-27 12:33:29 +0530

    Time: 542 ms (8.22%), Space: 44.4 MB (9.37%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,22 +1,58 @@
-class Solution 
-{
-  public:
-    int sumSubarrayMins(vector<int>& n) 
+class Solution {
+public:
+    vector<int>leftsmaller(vector<int>&arr)
     {
-        vector<long> s, sums(n.size(),0);
-        long j, res=0, mod = 1000000007;
-        for (int i = 0; i < n.size(); ++i)
+       int n=arr.size();
+       vector<int>lefts(n,1),rights(n,1);
+        stack<int>st;
+        for(int i=0; i<n; i++)
         {
-            while (!s.empty() && n[s.back()] > n[i])
-                s.pop_back();
-            j = !s.empty() ? s.back() : -1;
-            
-            sums[i] = ((j>=0?sums[j]:0) + (i-j)*n[i]) % mod;
-            s.push_back(i);
+            while(!st.empty() && arr[st.top()]>=arr[i])
+                st.pop();
+            int top=st.empty()?-1:st.top();
+            //cout<<top<<" "<<i-top<<endl;
+              lefts[i]=i-top;
+            st.push(i);
         }
-
-        for (int i = 0; i < sums.size(); ++i)
-            res = (res + sums[i]) % mod;
-        return res;
+        return lefts;
+    }
+    
+   vector<int>rightsmaller(vector<int>&arr)
+    {
+        int n=arr.size();
+       stack<int>st;
+       vector<int>rights(n,1);
+       for(int i=n-1; i>=0; i--)
+        {
+            while(st.size() && arr[i] < arr[st.top()]) 
+                st.pop();
+            if(st.size()) 
+                rights[i] = st.top() - i;
+            else
+                rights[i] = n - i;
+            st.push(i);
+        }
+       return rights;
+    }
+    int sumSubarrayMins(vector<int>& arr) {
+       int n=arr.size();
+       vector<int>lefts=leftsmaller(arr);
+       vector<int>rights=rightsmaller(arr);
+       int res = 0;
+        int MOD=1e9+7;
+        for(int i=0; i<n; i++)
+        {
+            long long prod = (lefts[i]*rights[i])%MOD;
+            prod = (prod*arr[i])%MOD;
+            res = (res + prod)%MOD;
+        }
+        for(auto i:lefts)
+            cout<<i<<" ";
+        cout<<endl;
+        for(auto i:rights)
+            cout<<i<<" ";
+        cout<<endl;
+        
+        return res%MOD;
     }
 };
