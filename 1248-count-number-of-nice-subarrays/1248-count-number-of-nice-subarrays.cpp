af81bcdf4c32f89a3d801d6b9cb87e66bbc27e67
Nishant Kumar <85111487+nk0129@users.noreply.github.com>
2023-01-02 23:58:35 +0530

    Time: 427 ms (27.68%), Space: 90.4 MB (33.36%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -10,11 +10,12 @@ public:
         }
         unordered_map<int,int>mp;
         int sum=0,cnt=0;
-        mp[0]=1;
+        //mp[0]=1;
         for(int i=0; i<nums.size(); i++)
         {
             sum+=nums[i];
-           
+            if(sum==k)
+                cnt++;
             if(mp.find(sum-k)!=mp.end())
                 cnt+=mp[sum-k];
             mp[sum]++;
