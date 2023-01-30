4743d696af5b20bf0b51e422693146a58a1e5565
Nishant Kumar <85111487+nishant0129@users.noreply.github.com>
2023-01-31 01:02:02 +0530

    Added solution - LeetHub

--- Minimum times A has to be repeated such that B is a substring of it - GFG/minimum-times-a-has-to-be-repeated-such-that-b-is-a-substring-of-it.cpp
+++ Minimum times A has to be repeated such that B is a substring of it - GFG/minimum-times-a-has-to-be-repeated-such-that-b-is-a-substring-of-it.cpp
@@ -8,30 +8,30 @@ using namespace std;
 
 class Solution {
   public:
-    int minRepeats(string a, string b) {
-        int len_a=a.size();
-        int len_b=b.size();
-        int ans=0;
-        for(int i=0;i<len_a;i++) {
-            int found=0,ct=1;
-            if(a[i]==b[0]) {
-                int l=i,r=0;
-                while(l<len_a and r<len_b and a[l++]==b[r++]) {
-                    if(l==len_a and r!=len_b) {
-                        // cout<<"HELLO  ";
-                        ct+=1;
-                        l=0;
-                    }
-                    if(r==len_b) {
-                        found=1;
-                    }
-                }
+    int minRepeats(string A, string B) {
+        int first_prefix=-1;
+        for(int i=0;i<A.size();i++) {
+            int j,k;
+            for(j=i,k=0;j<A.size() and k<B.size();j++,k++) {
+                if(A[j]!=B[k]) break;
             }
-            if(found) {
-                return ct;
+            if(k==B.size()) return 1;
+            if(j==A.size()) {
+                first_prefix=k;
+                break;
             }
         }
-        return -1;
+        if(first_prefix==-1) return -1;
+        int ans=1,i,j;
+        for(i=0,j=first_prefix;j<B.size();i++,j++) {
+            if(A[i]!=B[j]) return -1;
+            if(i==A.size()-1) {
+                i=-1;
+                ans++;
+            }
+        }
+        if(i!=0) return ans+1;
+        return ans;
     }
 };
 
