49792e8c2213a0f5b739738a9af8bcafaaa51e4e
Nishant Kumar <85111487+nishant0129@users.noreply.github.com>
2023-02-07 09:59:24 +0530

    Time: 172 ms (89.95%), Space: 66.6 MB (83.71%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool solve(vector<int>&arr,int m,int k,int mid)
+    {
+        int i=0,j=arr.size(),ct=0;
+        while(i<j)
+        {
+            if(arr[i]<=mid)
+            {
+                ct++;
+                if(ct==k)
+                {
+                    ct=0;
+                    m--;
+                    if(m==0)
+                        return true;
+                }
+            }
+            else
+            {
+                ct=0;
+            }
+            i++;
+        }
+        return false;
+    }
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        int l=1,h=1e9+1,ans=-1;
+        while(l<=h)
+        {
+            int mid=l+(h-l)/2;
+            if(solve(bloomDay,m,k,mid))
+            {
+                ans=mid;
+                h=mid-1;
+            }
+            else
+            {
+               l=mid+1;
+            }
+        }
+        return ans;
+    }
+};
