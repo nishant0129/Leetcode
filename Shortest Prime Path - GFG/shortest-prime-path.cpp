e018535d97c4d8e6e5df6193b39d54dce7d48382
Nishant Kumar <85111487+nishant0129@users.noreply.github.com>
2023-02-11 17:24:33 +0530

    Added solution - LeetHub

--- Shortest Prime Path - GFG/shortest-prime-path.cpp
+++ Shortest Prime Path - GFG/shortest-prime-path.cpp
@@ -25,13 +25,13 @@ class Solution{
     }
     int shortestPath(int num1,int num2)
     {   
-        // static vector<int>prime;
-        // if(prime.empty()) {
-        //     prime.assign(10000,true);
-        //     sieve(prime);
-        // }
-        vector<int>prime(100000,true);
-        sieve(prime);
+        static vector<int>prime;
+        if(prime.empty()) {
+            prime.assign(100000,true);
+            sieve(prime);
+        }
+        // vector<int>prime(100000,true);
+        // sieve(prime);
         vector<bool>visited(100000,false);
         queue<int>q;
         q.push(num1);
